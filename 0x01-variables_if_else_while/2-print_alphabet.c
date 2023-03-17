e058495ff081612f2ed9127d52f33c2af3191ffc
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-18 05:15:36 +1400

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: program that prints the alphabet in lowercase .
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar("%c\n",letter);
+
+	return (0);
+}
