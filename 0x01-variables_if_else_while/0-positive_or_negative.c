a18e44fbfd4a4a90071ae0641d5d67c56d2589c1
Sabriwwe <sabri00g@gmail.com>
2023-03-17 07:42:04 -0700

    start up

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 /*
- * main - program that detect positive and negative numbers.
+ * main - Entry point.
+ *
+ * Description: program that detect positive and negative numbers.
  *
  * Return: Always 0.
 */
@@ -14,11 +16,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if(n > 0)
-		printf("%d is positive\n", n);
-	else 
-		printf("%d is negative\n", n);
-	return (0);
+if ( n == 0 )
+printf("%d is zero\n", n);
+else if( n > 0 )
+printf("%d is positive\n", n);
+else 
+printf("%d is negative\n", n);
+return (0);
 }
