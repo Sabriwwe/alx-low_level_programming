ffd5907b8be04b86179eeb3547a07854554df63a
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-18 05:36:59 +1400

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,23 @@
 /**
 * main - Entry point
 *
-* Description: a program that prints the alphabet in lowercase and then in uppercase
+* Description: a program that prints the alphabet in
+*              lowercase and then in uppercase
 *
 * Return: Always 0.
 */
 
 int main(void)
 {
-  char let;
-  for (let = 'a'; let <= 'z'; let++)
-    putchar(let);
-  for (let = 'A'; let <= 'Z'; let++)
-    putchar(let);
-  putchar('\n');
-  return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
