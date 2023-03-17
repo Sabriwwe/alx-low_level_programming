b00cccf89b14922c73f6545d8e2fd94019156141
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-18 09:49:14 +1400

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,24 @@
 */
 int main(void)
 {
-int num;
-int num1;
-for (num = 0; num <= 9; num++)
-{
-for (num1 = 0; num1 <= 9; num1++)
-{
-putchar((num % 10) + '0');
-putchar((num1 % 10) + '0');
-if (num == 9 && num1 == 9)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');  
-return (0);
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
