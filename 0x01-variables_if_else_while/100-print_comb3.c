2089e70d826d0815114e7df2f421e014d845a4b6
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-18 07:27:49 +1400

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints all single digit numbers of base 10
+ * starting from 0, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num;
+int num1;
+for (num = 0; num <= 9; num++)
+{
+for (num1 = num + 2; num1 <= 9; num++)
+{
+putchar((num % 10) + '0');
+putchar((num1 % 10) + '0');
+if (num == 9 && num1 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
