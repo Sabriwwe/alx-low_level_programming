4e1c24a83e95284f27ca819e8f85422c5215c0f2
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-28 15:03:53 +1400

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 /**
  * main - Entry point
  *
- * Description: program that detect positive and negative numbers
+ * Description: a function that takes a pointer to an int
+ * parameter and updates the value it points to to 98.
  * @n: An integer input pointer
  */
 void reset_to_98(int *n)
