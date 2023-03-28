8135780a8e5bf6e0e46b17da22477d90f0e21092
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-28 14:53:51 +1400

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that takes a pointer to an int 
+ * as parameter and updates the value it points to to 98
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
