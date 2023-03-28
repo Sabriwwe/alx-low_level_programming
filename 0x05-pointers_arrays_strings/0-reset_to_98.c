f2f412fd633b5dcc763b6cd90a7ca4b24898f8d6
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-28 15:06:28 +1400

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 /**
  * main - Entry point
  *
- * Description: a function that takes a pointer to an int
+ * Description: A function that takes a pointer to an int
  * parameter and updates the value it points to to 98.
+ *
  * @n: An integer input pointer
  */
 void reset_to_98(int *n)
