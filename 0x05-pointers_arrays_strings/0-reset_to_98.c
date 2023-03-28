700ed67221602763b0eb1212e7aa0265bb9736fa
Sabriwwe <126092940+Sabriwwe@users.noreply.github.com>
2023-03-28 14:57:15 +1400

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - a function that takes a pointer to an int 
- * as parameter and updates the value it points to to 98
+ * main - Entry point .
  *
+ * Discription : a function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98
+ * 
  * Return: Always 0 (Succes)
  */
 
